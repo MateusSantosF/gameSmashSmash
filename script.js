adc5f8daf5e6ade33ac400398e7c62a73c1c1c3d
Mateus Santos <mateus.ferreira1@aluno.ifsp.edu.br>
2021-03-18 19:30:04 -0300

    v1.3 - finalizado

--- script.js
+++ script.js
@@ -15,13 +15,16 @@ function mudaDificuldade(){
     if( dificuldadeAtual == 'easy'){
         document.getElementById('btnDificuldade').textContent = 'hard';
     }else if(dificuldadeAtual == 'hard'){
-        document.getElementById('btnDificuldade').textContent = 'Insane';
+        document.getElementById('btnDificuldade').textContent = 'insane';
     }else{
         document.getElementById('btnDificuldade').textContent = 'easy';
     }
     
 }
 function iniciarPartida(){
+
+    dificuldadeAtual = document.getElementById('btnDificuldade').textContent;
+    document.getElementById('creditos').style.display = 'none';
     document.getElementById('titulo').style.display = 'none';
     document.getElementById('btnStart').style.display = 'none';
     document.getElementById('btnDificuldade').style.display = 'none';
@@ -30,10 +33,27 @@ function iniciarPartida(){
     document.getElementById('container-partida').style.visibility = 'inherit';
     document.getElementById('areaMorcegos').style.zIndex = '3';
     temporizador();
+    //pega dificuldade
 }
 
 function temporizador(){
     var minutos = 60;
+    var velocidadeMorcego;
+
+    switch(dificuldadeAtual){
+        case 'easy':
+            velocidadeMorcego = 1300;
+        break;
+        case 'hard':
+            velocidadeMorcego = 1000;
+        break;
+
+        case 'insane':
+            velocidadeMorcego = 680;
+        break;
+    }
+
+
     var tempo = setInterval(function(){
         minutos--;
         if(minutos == 0){
@@ -50,25 +70,39 @@ function temporizador(){
             location.replace("vitoria.html");
         }
 
-    }, 1000 ); 
+    }, velocidadeMorcego ); 
 }
 
 function morcegos(){
+   
+    var audio1 = document.querySelector(`audio[data-key = "slap"]`);
+    var audioVida = document.querySelector(`audio[data-key = "life"]`);
 
+    audio1.volume = 0.3;
+   
+    audioVida.volume = 0.4;
       //remover morcego a cada 1s
       if( document.getElementById('morcego') ){
         document.getElementById('morcego').remove();
+      
         nVidas--;      
         if(nVidas == 2){
+            audioVida.currentTime = 0;
+            audioVida.play();
             document.getElementById('barraVida').src = 'Imagens/vidaMetade.png';
-        }else if(nVidas == 1){
+        }else if(nVidas == 1){  
+            audioVida.currentTime = 0;
+            audioVida.play();
             document.getElementById('barraVida').src = 'Imagens/vidaVazia.png';
         }else{
+            audioVida.currentTime = 0;
+            audioVida.play();
             setTimeout(function(){
                 location.replace("derrota.html");
-            }, 1500);
+            }, 1000);
             
-        }    
+        }
+        audioVida.paused;    
     }
 
     var posicaoX = Math.abs(Math.floor(Math.random() * largura));
@@ -85,6 +119,8 @@ function morcegos(){
     morcego.style.cursor = 'pointer';
 
     morcego.onclick = function(){
+        audio1.currentTime = 0;
+        audio1.play();
         this.remove();
     }
     morcego.ondragstart = function(){ //bloqueia arrastar o morcego com o mouse
